1b54928e2f9a8b1288757c41aa1a399de508a39b
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2023-11-03 19:28:09 +0000

    config base axios

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,9 +1,10 @@
 import { boot } from "quasar/wrappers";
-import axios, { AxiosInstance } from "axios";
+import Axios, { AxiosInstance, AxiosStatic } from "axios";
+import { version } from "../../package.json";
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
-    $axios: AxiosInstance;
+    $axios: AxiosStatic;
     $api: AxiosInstance;
   }
 }
@@ -14,18 +15,78 @@ declare module "@vue/runtime-core" {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: process.env.API_URL });
+const axios = Axios.create({
+  baseURL: `${process.env.API_URL}/api`,
+  headers: { "Content-Type": "application/json" },
+});
 
 export default boot(({ app }) => {
+  // request
+  axios.interceptors.request.use((req) => {
+    // attach token to requests
+    // req.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`;
+
+    req.headers["X-Version"] = version;
+
+    return req;
+  }, Promise.reject);
+
+  // response
+  axios.interceptors.response.use(
+    ({ data }) => data,
+    async (error) => {
+      if (process.env.NODE_ENV === "local") {
+        console.log(error, error.response);
+      }
+
+      // un unexpected error ocurred
+      if (!error.response) {
+        return Promise.reject(error);
+      }
+
+      const { data, status } = error.response;
+
+      let errorMsg = data;
+
+      if (data.hasOwnProperty("error")) {
+        errorMsg = data.error;
+      } else if (data.hasOwnProperty("reason")) {
+        errorMsg = data.reason;
+      }
+
+      // not authenticated
+      if (status === 401) {
+        return app.config.globalProperties.$router.replace("login");
+      }
+
+      // no permissions for action
+      if (status === 403) {
+        //
+        console.error("no permissions");
+        return;
+      }
+
+      const { url, data: requestData } = error.config;
+
+      return Promise.reject({
+        error: errorMsg,
+        status,
+        request: { url, data: requestData },
+        response: error.response,
+        ...(data && { data }),
+      });
+    }
+  );
+
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$axios = Axios;
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = api;
+  app.config.globalProperties.$api = axios;
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
 });
 
-export { api };
+export { axios };
